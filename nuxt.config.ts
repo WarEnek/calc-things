8aa8e7bbd6bffa6e9891511c96f435b21e0c4139
Danil Efremov <danil.efremov@omnihr.co>
2024-10-01 21:40:43 +0400

    baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   app: {
-    baseURL: '/'
+  app: {
+    baseURL: '/calc-things/',
   },
   compatibilityDate: '2024-04-03',
   devtools: {enabled: true},
